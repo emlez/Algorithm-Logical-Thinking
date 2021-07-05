72772efe2516428a4e2364fc96b7696250c2f381
EmlSn <eml.snzrmz@gmail.com>
2021-07-05 12:08:05 -0500

    Programming in Python.

---
+++ Python/swapText.py
@@ -0,0 +1,10 @@
+def swap_text(s):
+  result = ''
+  for letter in s:
+    if letter == letter.upper():
+      result += letter.lower()
+    else:
+      result += letter.upper()
+  print(result)
+
+swap_text('Hola TODOS')
